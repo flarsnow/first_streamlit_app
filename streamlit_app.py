e0bfa290927956d1715d95847ad8a7d14ac83946
flarsnow <105606895+flarsnow@users.noreply.github.com>
2022-05-15 21:54:35 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Marents New Healthy Dinner')
+
+streamlit.header('Breakfast menu')
+streamlit.text('Omega 3 and Blueberry Oatmeal')
+streamlit.text('Kale, Spinnach and Rocket Smoothie')
+streamit.text('Hard-Boiled Free-Range Egg')
